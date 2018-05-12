0f74a5dd6409b29b69ac38f3246214a4a48c3369
SanHayet <498200332@qq.com>
2018-05-12 17:28:47 +0800

    @property.getter.setter

---
+++ nine_2.py
@@ -0,0 +1,35 @@
+#  @property 的使用，把方法转换成属性
+
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+        
+    @property
+    def height(self):
+        return self._height 
+        
+    @width.setter
+    def width(self, value):
+        self._width = value
+        
+    @height.setter
+    def height(self, value):
+        self._height = value
+        
+    @property
+    def resolution(self):
+        return self._width * self._height
+        
+s = Screen()
+
+s.width = 1024
+s.width(10)
+s.height = 768
+
+print("resolution = ", s.resolution)
+
+if s.resolution == 786432:
+    print("测试通过")
+else :
+    print("测试失败")
